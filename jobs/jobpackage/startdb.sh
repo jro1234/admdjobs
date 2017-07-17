15cfc725ddaf95b1d1469c1fd2cd51f35139dd1a
jrossyra <jrossyra@gmail.com>
2017-07-17 14:19:02 -0400

    Update startdb.sh

--- jobs/jobpackage/startdb.sh
+++ jobs/jobpackage/startdb.sh
@@ -4,33 +4,38 @@
 RUNNAME=$1
 N_WORKERS=$2
 
-
-DB_HOSTNAME=`ip addr show ipogif0 | grep -Eo '(addr:)?([0-9]*\.){3}[0-9]*'`
-echo "Host address: $DB_HOSTNAME"
-echo "The workers will try to find MongoDB here"
-
-echo "$DB_HOSTNAME" > $N_WORKERS.hostname
-
 export OMP_NUM_THREADS=16
 
-# Have to make config file specific to each database
-# so each has own socket
+# Parse the ip address of this node on Gemini
+# high-speed interconnect.
+DB_HOSTNAME=`ip addr show ipogif0 | grep -Eo '(addr:)?([0-9]*\.){3}[0-9]*'`
+echo "The workers will try to find MongoDB here"
+echo "Host address: $DB_HOSTNAME"
+
+
+# The workers will read this file to get
+# the address to this DB hosting node.
+echo "$DB_HOSTNAME" > $N_WORKERS.hostname
+
+# Making config file specific to each database
+# so each has own socket. Can run concurrent DB
+# independently (for different jobs, etc).
 mkdir ${ADMD_DB}data/$DB_HOSTNAME/
 echo -e "net:\n   unixDomainSocket:\n      pathPrefix: ${ADMD_DB}data/$DB_HOSTNAME/\n   bindIp: 0.0.0.0" > ${ADMD_DB}mongo.$RUNNAME-$N_WORKERS-$DB_HOSTNAME.cfg 
 
 if [ $N_WORKERS -gt 400 ]; then
-  # this may read higher than the system hard limit,
+  # this may evaluate to higher than the system hard limit,
   # which will take effect and prevent the remaining
-  # worker connections
+  # worker connections... be careful to avoid waste
   maxconns=$((40+2*$N_WORKERS))
   ulim=`echo "$maxconns*1.2" | bc`
   ulimit -n ${ulim%.*}
 else
-  # mongodb default
+  # mongodb default for typical file ulimit of 1024
   maxconns=819
 fi
 
-# Making database for each mongod
+# Making database (ie directory) for this mongod instance
 mkdir ${ADMD_DB}data/$RUNNAME.$N_WORKERS.db/
-numactl --interleave=all mongod --maxConns $maxconns --config ${ADMD_DB}mongo.$RUNNAME-$N_WORKERS-$DB_HOSTNAME.cfg --dbpath ${ADMD_DB}data/$RUNNAME.$N_WORKERS.db/ --verbose &> mongo.$RUNNAME.$N_WORKERS.log
+numactl --interleave=all mongod --maxConns $maxconns --config ${ADMD_DB}mongo.$RUNNAME-$N_WORKERS-$DB_HOSTNAME.cfg --dbpath ${ADMD_DB}data/$RUNNAME.$N_WORKERS.db/ &> mongo.$RUNNAME.$N_WORKERS.log
 
