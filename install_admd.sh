6954e1a1458046f9f415818c1f49ed8d984cd75a
jrossyra <jrossyra@gmail.com>
2017-10-05 14:58:22 -0400

    install script update

--- install_admd.sh
+++ install_admd.sh
@@ -9,6 +9,10 @@ INSTALL_ADMD_DATA=$MEMBERWORK/bip149/
 INSTALL_ADMD_JOBS=$MEMBERWORK/bip149/
 INSTALL_ADMD_DB=$PROJWORK/bip149/$USER/
 
+FOLDER_ADMD_DB=mongodb
+FOLDER_ADMD_DATA=admd
+FOLDER_ADMD_JOBS=admd
+
 ## Options & Version configuration stuff:
 ADAPTIVEMD_VERSION=jrossyra/adaptivemd.git
 ADAPTIVEMD_BRANCH=rp_integration
@@ -31,15 +35,15 @@ echo "Installing Mongo in: $INSTALL_ADMD_DB"
 curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-$MONGODB_VERSION.tgz
 tar -zxvf mongodb-linux-x86_64-$MONGODB_VERSION.tgz
 mkdir mongodb
-mv mongodb-linux-x86_64-$MONGODB_VERSION/ mongodb/
-mkdir -p mongodb/data/db
+mv mongodb-linux-x86_64-$MONGODB_VERSION/ $FOLDER_ADMD_DB
+mkdir -p ${FOLDER_ADMD_DB}/data/db
 rm mongodb-linux-x86_64-$MONGODB_VERSION.tgz
 echo "# APPENDING PATH VARIABLE with AdaptiveMD Environment" >> ~/.bashrc
-echo "export ADMD_DB=${INSTALL_ADMD_DB}mongodb/" >> ~/.bashrc
-echo "export PATH=${INSTALL_ADMD_DB}mongodb/mongodb-linux-x86_64-$MONGODB_VERSION/bin/:\$PATH" >> ~/.bashrc
+echo "export ADMD_DB=${INSTALL_ADMD_DB}${FOLDER_ADMD_DB}/" >> ~/.bashrc
+echo "export PATH=${INSTALL_ADMD_DB}${FOLDER_ADMD_DB}/mongodb-linux-x86_64-$MONGODB_VERSION/bin/:\$PATH" >> ~/.bashrc
 echo "Done installing Mongo, appended PATH with mongodb bin folder"
 # Mongo should default to using /tmp/mongo-27017.sock as socket
-echo -e "net:\n   unixDomainSocket:\n      pathPrefix: ${INSTALL_ADMD_DB}mongodb/data/\n   bindIp: 0.0.0.0" > ${INSTALL_ADMD_DB}mongodb/mongo.cfg
+echo -e "net:\n   unixDomainSocket:\n      pathPrefix: ${INSTALL_ADMD_DB}${FOLDER_ADMD_DB}/data/\n   bindIp: 0.0.0.0" > ${INSTALL_ADMD_DB}${FOLDER_ADMD_DB}/mongo.cfg
 source ~/.bashrc
 echo "MongoDB daemon installed here: "
 which mongod
@@ -57,7 +61,11 @@ echo "Miniconda conda executable here: "
 echo "export CONDAPATH=${INSTALL_CONDA}miniconda$CONDA_VERSION/bin" >> ~/.bashrc
 which conda
 conda install python
-conda install ujson pyyaml numpy pymongo=$PYMONGO_VERSION pyemma openmm=$OPENMM_VERSION mdtraj
+
+# TODO this is redundant with AdaptiveMD install
+#      remove after test
+#conda install ujson pyyaml numpy pymongo=$PYMONGO_VERSION pyemma openmm=$OPENMM_VERSION mdtraj
+
 rm Miniconda$CONDA_VERSION-latest-Linux-x86_64.sh
 
 ###############################################################################
@@ -73,7 +81,7 @@ python -c "import adaptivemd" || echo "something wrong with adaptivemd install"
 echo "export ADAPTIVEMD=${INSTALL_ADAPTIVEMD}adaptivemd/" >> ~/.bashrc
 ## TEST AdaptiveMD
 echo "Starting database for tests"
-mongod --config ${INSTALL_ADMD_DB}mongodb/mongo.cfg --dbpath ${INSTALL_ADMD_DB}mongodb/data/db/ --verbose > ${INSTALL_ADMD_DB}mongodb/data/mongo.install.log &
+mongod --config ${INSTALL_ADMD_DB}${FOLDER_ADMD_DB}/mongo.cfg --dbpath ${INSTALL_ADMD_DB}${FOLDER_ADMD_DB}/data/db/ --verbose > ${INSTALL_ADMD_DB}${FOLDER_ADMD_DB}/data/mongo.install.log &
 MONGO_PID=$!
 cd adaptivemd/tests/
 echo "Testing AdaptiveMD with python version: "
@@ -88,11 +96,11 @@ kill $MONGO_PID
 #
 ###############################################################################
 cd $INSTALL_ADMD_DATA
-mkdir admd/
+mkdir $FOLDER_ADMD_DATA
 echo "projects and workers subfolders will be created"
 echo "by AdaptiveMD inside this directory:"
-echo "  `pwd`/admd/"
-echo "export ADMD_DATA=${INSTALL_ADMD_DATA}admd/" >> ~/.bashrc
+echo "  `pwd`/${FOLDER_ADMD_DATA}/"
+echo "export ADMD_DATA=${INSTALL_ADMD_DATA}${FOLDER_ADMD_DATA}/" >> ~/.bashrc
 
 ###############################################################################
 #
@@ -100,11 +108,9 @@ echo "export ADMD_DATA=${INSTALL_ADMD_DATA}admd/" >> ~/.bashrc
 #
 ###############################################################################
 cd $INSTALL_ADMD_JOBS
-mkdir admd/
-cd admd
-cp -r $CWD/jobs/ ./
-mkdir jobs/
-echo "export ADMD_JOBS=${INSTALL_ADMD_JOBS}admd/jobs/" >> ~/.bashrc
+mkdir $FOLDER_ADMD_JOBS
+cd $FOLDER_ADMD_JOBS
+echo "export ADMD_JOBS=${INSTALL_ADMD_JOBS}${FOLDER_ADMD_JOBS}/jobs/" >> ~/.bashrc
 source ~/.bashrc
 cd $CWD
 echo "if no errors then AdaptiveMD & dependencies installed"
